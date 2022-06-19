e3c2431ea3b04e7d193b5270e56ee258b87ae6a3
on1x22 <on1x22@mail.ru>
2022-06-19 13:00:09 +0400

    first commit

---
+++ TestProjectForGitHub/Geometry.cs
@@ -0,0 +1,25 @@
+﻿namespace TestProjectForGitHub
+{
+    public class Geometry
+    {
+        public int GetPerimeter(int a, int b)
+        {
+            return 2 * a + 2 * b;
+        }
+
+        public int RectangleArea(int a, int b)
+        {
+            return a * b;
+        }
+
+        public string GetStatistic(int a, int b)
+        {
+            return "Периметр = " + GetPerimeter(a, b) + ", площадь = " + RectangleArea(a, b);
+        }
+
+        public override string  ToString()
+        {
+            return "Geometry class";
+        }
+    }
+}
